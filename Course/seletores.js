9c85fae9bb69748cfc8a442e8b338040980bbf1f
Alberto <betim009@gmail.com>
2024-04-29 21:03:13 -0300

    conteudo

---
+++ Course/seletores.js
@@ -0,0 +1,15 @@
+//nome da variavel       resgatando do DOM o elemento por classe
+const h2Class = document.getElementsByClassName('h2-title');
+//nome da variavel       resgatando do DOM o elemento por classe
+const h2AllSelector = document.querySelectorAll('.h2-title');
+
+// nome da variavel           resgatando do DOM o elemento por id
+const h3Id = document.getElementById('h3-title');
+// nome da variavel           resgatando do DOM o elemento por id
+const h3Selector = document.getElementById('#h3-title');
+
+// Quando usar AllSelector e ElementsClassName ? 
+// R: Quando houver necessidade de resgatar mais de 1 um elemento
+
+// Quando usar selector e ElementId ?
+// R: Quando houver necessidade de apenas um unico elemento
---
+++ Project/README.md
@@ -0,0 +1,18 @@
+# project-fly
+
+
+
+
+## Checklist
+
+### Data.js
+1. [ ] Criar mais cidades dentro da base de dados
+2. [ ] Criar uma função de busca de dados expecificos 
+
+
+
+### Git GitHub
+   1. `git add .` Utilizado para adicionar novas atulizações do projeto.
+   2. `git commit -m "a alteração dentro do projeto/repositorio"` Utilizado para descrever o que foi atualizado.
+   3. `git push` Utilizado para enviar para o GitHub.
+   4. `git pull` Utilizado para atualizar do GitHub para o seu Desktop.
