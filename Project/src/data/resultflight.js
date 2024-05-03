d2d01595b965a0aba81e4624266a8d5072e4126c
njfw50 <139028080+njfw50@users.noreply.github.com>
2024-05-02 20:11:43 -0400

    resultflight

---
+++ Project/src/data/resultflight.js
@@ -0,0 +1,14 @@
+const resultflight = [
+    {
+        Origem:"Rio de Janeiro", [GIG-Galeao];
+        Destino:"São Paulo", [GRU-Guarulhos];
+        Data de Ida:"07/20/2024";
+        Data de Retorno:"08/20/2024";
+        Hora de Saida:"03:00pm";
+        Hora de Chagada:"04:25pm";
+        Valor:360;
+        Moeda:"USD";
+        Maletas:"Artigos Pessoais:1pc 10kg";
+        Companhia Aerea:"LATAM"
+    }
+]
